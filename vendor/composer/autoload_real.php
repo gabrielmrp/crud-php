7a1c2550578cc50cd62d3d993cecaa297c1cdecf
gabrielmrp <gabrielmrdp@gmail.com>
2021-02-17 22:16:15 -0300

    Composer.lock

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit6018c9340d64040e7368de6b0347d653
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit6018c9340d64040e7368de6b0347d653::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire6018c9340d64040e7368de6b0347d653($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire6018c9340d64040e7368de6b0347d653($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
